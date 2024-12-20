e8ca007b8bfb9b34ec91527d05e61832e39f1ae5
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-20 23:05:57 +0530

    Fix logo placement on light and dark mode

--- frontend/components/layout/header.tsx
+++ frontend/components/layout/header.tsx
@@ -40,15 +40,15 @@ export function Header() {
 
   return (
     <header className="fixed w-full top-0 z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-1">
-      <div className="flex h-14 items-center justify-between px-4 md:px-8">
+      <div className="flex h-14 items-center justify-between px-4 md:px-6">
         <Link
           href={isLoggedIn ? "/dashboard" : "/"}
           className="flex items-center space-x-2"
         >
-          <div className="block dark:hidden w-48 h-10">
+          <div className="block ml-2 dark:hidden w-48 h-10">
             <LogoBlack />
           </div>
-          <div className="hidden dark:block w-48 h-10">
+          <div className="hidden md:-ml-6 dark:block w-48 h-10">
             <LogoWhite />
           </div>
         </Link>
