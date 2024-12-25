92a6b843b069bbd6e8579d54b7beb9e07793ca07
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-25 14:01:16 +0530

    Update server's cors configuration to properly handle credentials

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const startServer = async () => {
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-      exposedHeaders: ["set-cookie"],
+      exposedHeaders: ["Set-Cookie"],
     })
   );
 
